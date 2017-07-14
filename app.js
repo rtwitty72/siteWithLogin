9202b1b32a9548d9dc0923f4cbf6976d7e8536d7
MacBook Pro <macbookpro@MacBooks-MacBook-Pro-2.local>
2017-07-14 13:53:52 -0400

    updated to show welcome screen

--- app.js
+++ app.js
@@ -43,11 +43,7 @@ app.use(function(req, res, next){
   var pathname = parseurl(req).pathname;
 
   if(!req.session.user && pathname != '/login'){
-
-  if (pathname != '/login'){
-    let qs = pathname != '/login' ? "?next=" + pathname : '';;
-  }
-    res.redirect('/login' + qs);
+    res.redirect('/login?next=' + pathname);
   }else{
     next();
   }
@@ -60,26 +56,24 @@ app.use(function(req, res, next){
     views = req.session.views = {};
   }
 
-  var pathname = parseurl(req).pathname;
+var pathname = parseurl(req).pathname;
 
-  views[pathname] = (views[pathname] || 0) +1;
-
-  next();
+views[pathname] = (views[pathname] || 0) + 1;
+next();
 });
 
 app.get('/login', function(req, res){
+
   var context = {
     next: req.query.next
   };
-
+    res.render('login', context);
   if(req.session.user){
-
+    res.redirect('/welcome');
   }
-
-  res.render('login', context);
 });
 
-app.get('/', function(req, res){
+app.get('/login', function(req, res){
   var context = {
     'username' : req.session.user.username,
     'views' : req.session.views['/']
@@ -87,40 +81,30 @@ app.get('/', function(req, res){
   res.render('index', context);
 });
 
+
+
 app.post('/login', function(req, res){
   var username = req.body.username;
   var password = req.body.password;
-  var nextPage = req.body.next || '/';
+  var nextPage = req.body.next;
 
   var person = users.find(function(user){
     return user.username == username;
   });
-
-if (person && person.password == password){
-  req.session.user = person;
-}else if (req.session.user) {
-  delete req.session.user;
-}
-
-if(req.session.user){
-  res.redirect(nextPage);
-}else {
-  res.redirect('/login');
-}
-
+  if(person && person.password == password){ //checking for valid login
+    req.session.user = person;
+  }else if(req.session.user) {
+    delete req.session.user;
+  }
+  if(req.session.user){
+    res.redirect('/welcome'); //user will be sent to welcom screen if login is valid
+  }else{
+    res.redirect('/login');
+  }
 });
 
-app.post('/number', function(req, res){
-  var context = {
-    'views' : req.session.views['/'] + 1
-  };
-  res.redirect('/');
+app.get('/welcome', function(req, res){
+  res.send("Welcome to my site " + req.session.user.username + " !");
 });
-
-app.post('/logout', function(req, res){
-  delete req.session.user;
-  res.redirect('/login');
-});
-
 app.listen(3000);
 console.log('Application has been initialized');
